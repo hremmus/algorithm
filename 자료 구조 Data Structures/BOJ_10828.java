3365055b6b2fba92ef1d2ea27a157156697edff7
hremmus <hremmus@gmail.com>
2023-01-02 23:42:22 +0900

    스택

--- 자료 구조 Data Structures/BOJ_10828.java
+++ 자료 구조 Data Structures/BOJ_10828.java
@@ -5,16 +5,64 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+class myStack<Integer> {
+  class Node {
+    public Node(int data) {
+      this.data = data;
+    }
+
+    int data;
+    Node next;
+  }
+
+  Node top;
+  int size;
+
+  public void push(int item) {
+    Node newNode = new Node(item);
+    // 기존 top에 연결
+    newNode.next = top;
+    top = newNode;
+    size++;
+  }
+
+  public int pop() {
+    if (size == 0) {
+      return -1;
+    } else {
+      int item = top.data; // LIFO(Last In First Out)
+      top = top.next; // 꺼낸 노드의 연결을 끊음(= 맨 위 항목 제거)
+      size--;
+      return item;
+    }
+  }
+
+  public int top() { // Stack.peek()
+    if (top == null) {
+      return -1;
+    } else {
+      return top.data;
+    }
+  }
+
+  public int empty() {
+    return top == null ? 1 : 0;
+  }
+
+  public int size() {
+    return size;
+  }
+}
+
 public class Main {
   static int N;
-  static int[] stack;
-  static int size = 0; // element의 개수를 집계할 변수가 필요
 
   public static void main(String[] args) throws IOException {
+    /* 스택의 구조를 재현한 myStack */
+    myStack<Integer> stack = new myStack<>();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // System.out보다 빠름
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     N = Integer.parseInt(br.readLine());
-    stack = new int[N];
 
     StringTokenizer st;
     while (N-- > 0) {
@@ -22,22 +70,22 @@ public class Main {
       String command = st.nextToken();
       switch (command) {
         case "push":
-          push(Integer.parseInt(st.nextToken()));
+          stack.push(Integer.parseInt(st.nextToken()));
           break;
         case "pop":
-          bw.write(String.valueOf(pop())); // int 값을 그대로 넣으면 ASCII 코드에 따른 char형 값으로 변환
-          bw.newLine(); // 줄바꿈 '\n'이 포함
+          bw.write(String.valueOf(stack.pop()));
+          bw.newLine();
           break;
         case "size":
-          bw.write(String.valueOf(size()));;
+          bw.write(String.valueOf(stack.size()));;
           bw.newLine();
           break;
         case "empty":
-          bw.write(String.valueOf(empty()));
+          bw.write(String.valueOf(stack.empty()));
           bw.newLine();
           break;
         case "top":
-          bw.write(String.valueOf(top()));
+          bw.write(String.valueOf(stack.top()));
           bw.newLine();
           break;
       }
@@ -46,37 +94,5 @@ public class Main {
     bw.flush();
     br.close();
     bw.close();
-  }
-
-  static void push(int element) {
-    stack[size] = element;
-    size++;
-  }
-
-  static int pop() {
-    if (size == 0) {
-      return -1;
-    } else {
-      int element = stack[size - 1];
-      stack[size - 1] = 0;
-      size--;
-      return element;
-    }
-  }
-
-  static int size() {
-    return size;
-  }
-
-  static int empty() {
-    return size == 0 ? 1 : 0;
-  }
-
-  static int top() {
-    if (size == 0) {
-      return -1;
-    } else {
-      return stack[size - 1];
-    }
   }
 }
