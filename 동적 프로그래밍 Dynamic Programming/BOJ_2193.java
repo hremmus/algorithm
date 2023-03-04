1702e5e6274fb39d91c922554c2b02d9aea9c068
hremmus <hremmus@gmail.com>
2023-03-04 22:25:54 +0900

    이친수

--- 동적 프로그래밍 Dynamic Programming/BOJ_2193.java
+++ 동적 프로그래밍 Dynamic Programming/BOJ_2193.java
@@ -3,38 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-  // 2차원 배열 + 재귀 함수로 풀이
+  // 1차원 배열과 반복문으로 풀이
   static int N;
-  static Long dp[][];
+  static long dp[];
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine()); // 1 <= N <= 90 자릿수 ==> long
+    N = Integer.parseInt(br.readLine()); // 1 <= N <= 90 자릿수 => long
     // 1: 1 ==> 1
     // 2: 10 ==> 1
     // 3: 100, 101 ==> 2
     // 4: 1000, 1001, 1010 ==> 3
     // 5: 10000, 10001, 10010, 10100, 10101 ==> 5
-    dp = new Long[N + 1][2];
-    dp[1][0] = 0L; dp[1][1] = 1L; // digit == 1일 때 값을 초기화해놓아야 재귀 함수 호출이 가능
-    
-    int result = 0;
-    for (int i = 0; i <= 1; i++) {
-      result += recursive(N, i);
+    dp = new long[N + 1];
+    dp[1] = 1;
+
+    // 4자릿수 10'00', 10'01', 10'10' ==> 뒷자리가 3자릿수 2개와 겹침, 2자릿수 1개와 겹침
+    // 5자릿수 10'000', 10'001', 10'010', 10'100', 10'101' ==> 뒷자리가 4자릿수 3개와 겹침, 3자릿수 2개와 겹침
+    // => 아래의 점화식이 성립한다
+    for (int i = 2; i < dp.length; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
     }
-    
-    System.out.println(result);
+
+    System.out.println(dp[N]);
     br.close();
-  }
-  
-  static long recursive(int digit, int value) {
-    if (digit == 1) return dp[digit][value];
-    
-    if (dp[digit][value] == null) {
-      if (value == 1) dp[digit][value] = recursive(digit - 1, 0);
-      if (value == 0) dp[digit][value] = recursive(digit - 1, 0) + recursive(digit - 1, 1);
-    }
-    
-    return dp[digit][value];
   }
 }
