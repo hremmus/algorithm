3afe2da47c82e0eea865cd433b1260077136884c
hremmus <hremmus@gmail.com>
2023-03-12 23:57:48 +0900

    RGB거리

--- 동적 프로그래밍 Dynamic Programming/BOJ_1149.java
+++ 동적 프로그래밍 Dynamic Programming/BOJ_1149.java
@@ -4,15 +4,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  // 1) 2차원 배열 한 개 + 반복문 이용
-  
+  // 2) 2차원 배열 두 개 + 재귀 함수 이용
+
   // 각 집의 비용의 최솟값을 찾아 누적합을 구하면 오답
   // 인접한 집의 색이 겹치지 않도록 하는 모든 경로의 경우의 수를 찾은 다음 최종적으로 최소인 누적합을 찾아야 한다.
   final static int RED = 0;
   final static int GREEN = 1;
   final static int BLUE = 2;
   static int N;
-  static int[][] cost;
+  static int[][] cost, dp;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +20,7 @@ public class Main {
 
     N = Integer.parseInt(br.readLine()); // 1 <= N <= 1000 (입력 2 <= N <= 1000)
     cost = new int[N][3];
+    dp = new int[N][3];
 
     for (int i = 0; i < cost.length; i++) {
       st = new StringTokenizer(br.readLine());
@@ -36,13 +37,27 @@ public class Main {
     // N번 집 cost[N][0] += Math.min(cost[N - 1][1], cost[N - 1][2])
     //       cost[N][1] += Math.min(cost[N - 1][0], cost[N - 1][2])
     //       cost[N][2] += Math.min(cost[N - 1][0], cost[N - 1][1])
-    for (int i = 1; i < cost.length; i++) {
-      cost[i][RED] += Math.min(cost[i - 1][GREEN], cost[i - 1][BLUE]);
-      cost[i][GREEN] += Math.min(cost[i - 1][RED], cost[i - 1][BLUE]);
-      cost[i][BLUE] += Math.min(cost[i - 1][RED], cost[i - 1][GREEN]);
+    // 함수를 호출하기 전, dp[0]의 값을 첫번째 집의 색칠 비용으로 초기화
+    dp[0][RED] = cost[0][RED];
+    dp[0][GREEN] = cost[0][GREEN];
+    dp[0][BLUE] = cost[0][BLUE];
+
+    System.out.println(Math.min(recursive(N - 1, RED), Math.min(recursive(N - 1, GREEN), recursive(N - 1, BLUE))));
+    br.close();
+  }
+
+  static int recursive(int N, int color) {
+    if (dp[N][color] == 0) {
+      // 이전 집의 최소 비용을 구한 값에 현재 집의 비용을 더함
+      if (color == RED) {
+        dp[N][RED] = Math.min(recursive(N - 1, GREEN), recursive(N - 1, BLUE)) + cost[N][RED];
+      } else if (color == GREEN) {
+        dp[N][GREEN] = Math.min(recursive(N - 1, RED), recursive(N - 1, BLUE)) + cost[N][GREEN];
+      } else if (color == BLUE) {
+        dp[N][BLUE] = Math.min(recursive(N - 1, RED), recursive(N - 1, GREEN)) + cost[N][BLUE];
+      }
     }
 
-    System.out.println(Math.min(Math.min(cost[N - 1][RED], cost[N - 1][GREEN]), cost[N - 1][BLUE]));
-    br.close();
+    return dp[N][color];
   }
 }
