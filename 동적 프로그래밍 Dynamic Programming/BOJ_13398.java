a9ff23d01a7c166447a6be2a1ec9503e3f252fc1
hremmus <hremmus@gmail.com>
2023-03-22 23:47:06 +0900

    연속합 2

--- 동적 프로그래밍 Dynamic Programming/BOJ_13398.java
+++ 동적 프로그래밍 Dynamic Programming/BOJ_13398.java
@@ -4,10 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  // 1) 1차원 배열로 풀이
-  // BOJ_1912 응용, 제거되는 수를 고려해야 하므로 양방향에서 가장 큰 연속합을 구하고, 그 결과를 짝지어 더함
+  // 2) 2차원 배열로 풀이
   static int n;
-  static int[] input, dp_r, dp_l;
+  static int[] input;
+  static int[][] dp;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,26 +19,16 @@ public class Main {
       input[i] = Integer.parseInt(st.nextToken());
     }
 
+    dp = new int[n][2]; // 0: 제거X 1: 제거O 상태일 때 구해지는 최대 연속합
+    dp[0][0] = dp[0][1] = input[0];
+
     int max = input[0];
-
-    // ======================>
-    dp_r = new int[n];
-    dp_r[0] = input[0];
     for (int i = 1; i < n; i++) {
-      dp_r[i] = Math.max(input[i], dp_r[i - 1] + input[i]);
-      max = Math.max(max, dp_r[i]); // 수를 제거하지 않고 연속합의 최댓값 도출
-    }
+      dp[i][0] = Math.max(input[i], dp[i - 1][0] + input[i]);
+      // 1) i번째 원소가 지워지는 경우 2) i번쨰 이전 원소가 지워진 경우
+      dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + input[i]);
 
-    // <======================
-    dp_l = new int[n];
-    dp_l[n - 1] = input[n - 1];
-    for (int i = n - 2; i >= 0; i--) {
-      dp_l[i] = Math.max(input[i], dp_l[i + 1] + input[i]);
-    }
-
-    // 수열에서 특정한 수를 하나 제거하면 아래와 같은 식을 세울 수 있다.
-    for (int i = 1; i < n - 1; i++) {
-      max = Math.max(max, dp_r[i - 1] + dp_l[i + 1]); // 제거하기 전 최댓값과 비교-선택
+      max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
     }
 
     System.out.println(max);
