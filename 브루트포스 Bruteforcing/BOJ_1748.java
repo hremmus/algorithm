dce8a91970cfcdc21c401f3c102ac3af973c12f0
hremmus <hremmus@gmail.com>
2023-04-02 23:24:57 +0900

    수 이어 쓰기 1

--- 브루트포스 Bruteforcing/BOJ_1748.java
+++ 브루트포스 Bruteforcing/BOJ_1748.java
@@ -10,13 +10,10 @@ public class Main {
     N = Integer.parseInt(br.readLine()); // 1 <= N <= 100000000
 
     int count = 0;
-    for (int start = 1, length = 1; start <= N; start *= 10, length++) {
-      // start = 1이면 end = 9, start = 10이면 end = 99, start = 100이면, end = 999
-      int end = (start * 10) - 1;
-      // end가 N을 넘어서는 안 됨
-      if (end > N) end = N;
-      
-      count += (end - start + 1) * length; // 범위에 속하는 개수에 자릿수를 곱한 다음 합산
+    // 1부터 시작, 10의 거듭제곱으로 증가. 숫자 N보다 커지면 종료(=N의 자릿수만큼 반복)
+    for (int i = 1; i <= N; i *= 10) {
+      count += (N - i) + 1; // 자릿수의 누적함
+      // N + (N - 10) + 1 + (N - 100) + 1 + ... + (N - N의 자릿수) + 1
     }
 
     System.out.println(count);
