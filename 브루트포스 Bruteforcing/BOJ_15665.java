3ec76c2f13aa42728267cbcd89b995eae9284fc8
hremmus <hremmus@gmail.com>
2023-04-14 11:46:45 +0900

    N과 M (11)

---
+++ 브루트포스 Bruteforcing/BOJ_15665.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+  static int N, M;
+  static int[] input, result;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] line = br.readLine().split(" ");
+
+    N = Integer.parseInt(line[0]); // 원소의 개수 1 <= N <= 7
+    M = Integer.parseInt(line[1]); // 수열의 길이 1 <= M <= N <= 7
+
+    input = new int[N];
+    result = new int[M];
+
+    line = br.readLine().split(" ");
+    for (int i = 0; i < N; i++) {
+      input[i] = Integer.parseInt(line[i]); // 1 <= input[i] <= 10000
+    }
+    Arrays.sort(input);
+
+    dfs(0);
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  private static void dfs(int depth) {
+    if (depth == M) {
+      for (int element : result) {
+        sb.append(element).append(' ');
+      }
+      sb.append('\n');
+      return;
+    }
+
+    int before = 0;
+    for (int i = 0; i < N; i++) { // input[index]를 탐색
+      // N개의 입력 받은 수 중에는 같은 수가 있을 수 있다.
+      if (before != input[i]) { // 중복 수열 출력 방지
+        result[depth] = input[i];
+        before = input[i]; // 이전 원소로 지정
+        dfs(depth + 1); // 다음 원소 출력
+      }
+    }
+  }
+}
